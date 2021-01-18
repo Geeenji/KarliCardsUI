9fb7584a374dd776ff9d388e235b320df66a2b35
TengfeiWong <1392281552@qq.com>
2021-01-18 17:34:44 +0800

    Add project files.

---
+++ Ch13CardLib/ComputerSkillLevel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch13CardLib
+{
+    [Serializable]
+    public enum ComputerSkillLevel
+    {
+        Dumb,
+        Good,
+        Cheats,
+        bad,
+        terrible,
+        catastrophy
+       
+    }
+}
---
+++ KarliCards.Gui/OptionsWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using System.Windows;
+using System.Xml.Serialization;
+using Ch13CardLib;
+
+namespace KarliCards.Gui
+{
+    public partial class OptionsWindow : Window
+    {
+        private GameOptions gameOptions;
+
+        public OptionsWindow()
+        {
+            gameOptions = GameOptions.Create();
+            DataContext = gameOptions;
+            InitializeComponent();
+        }
+
+
+        private void dumbAIRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            gameOptions.ComputerSkill = ComputerSkillLevel.Dumb;
+        }
+
+        private void goodAIRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            gameOptions.ComputerSkill = ComputerSkillLevel.Good;
+        }
+
+        private void cheatingAIRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            gameOptions.ComputerSkill = ComputerSkillLevel.Cheats;
+        }
+        private void badAIRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            gameOptions.ComputerSkill = ComputerSkillLevel.bad;
+        }
+        private void terribleAIRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            gameOptions.ComputerSkill = ComputerSkillLevel.terrible;
+        }
+        private void catastrophyRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            gameOptions.ComputerSkill = ComputerSkillLevel.catastrophy;
+        }
+        private void okButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            gameOptions.Save();
+            Close();
+        }
+
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            gameOptions = null;
+            Close();
+        }
+
+    }
+}
